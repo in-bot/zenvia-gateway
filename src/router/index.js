4384020e6132d6f5bd63f5d873e9958ecd012315
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2023-12-06 17:51:25 -0300

    Initial version

--- src/router/index.js
+++ src/router/index.js
@@ -7,5 +7,10 @@ router.get("/bot",);
 router.post("/bot",);
 router.get("/webhook",);
 router.post("/webhook", new WebhookController().getWebhookMessage);
+router.get("/version",(req, res, next) => {
+    console.log(new Date(), 'Version request received', version);
+    res.send({ version: version.num });
+    next();
+});
 
 module.exports = router
